0b9ce6aff7cf39ef3fdee33e883b62e3731a0965
benicinia <92032482+benicinia@users.noreply.github.com>
2022-04-21 19:03:41 +0300

    Add files via upload

---
+++ sfpdb.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+var bodyparser = require('body-parser');
+const Mysqli = require('mysqli');
+var app = express();
+
+
+var mysql      = require('mysql');
+
+var pool  = new mysql.createPool({
+  connectionLimit : 10,
+  host            : '127.0.0.1',
+  user            : 'root',
+  password        : '',
+  database        : 'sfp'
+});
+
+
+
+
+
+pool.getConnection(function(err, connection) {
+  //if (err) throw err; // not connected!
+  if(!err==true){
+  try {console.log('Connected to sexy feet pussy network!');
+  pool.on('release', function (connection) {
+    console.log('Connection %d released', connection.threadId);
+  });
+}
+  catch (err) {console.log('not connected!');}}
+   
+});
+
+
+
+module.exports = pool;
+
